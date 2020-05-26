ca0e2a1b464573d45e2319a80b7e1ffef4896433
AKornilov <akornilov@beeline.kz>
2020-05-26 16:22:25 +0600

    remove comment code

--- MessageRead.js
+++ MessageRead.js
@@ -173,100 +173,14 @@
                     showNotification('Ошибка создания МОМ встречи', state + ': ' + message);
                 }
             });
-
-            //showNotification('МОМ встречи создан', '<a target="_blank" href="https://confluence.beeline.kz/pages/viewpage.action?pageId=53812347">confluence</a>');
         });
-
-        //Office.context.ui.displayDialogAsync('https://office.beeline.kz/sites/system/Lists/TMP/NewForm.aspx?Title=' + item.subject);
-
-        //var sendData = {
-        //    pageId: "53811457",
-        //    f: "meetingCollector",
-        //    title01: item.subject,
-        //    beginTm: item.start.format('dd.MM.yyyy HH:mm'),
-        //    endTm: item.end.format('dd.MM.yyyy HH:mm'),
-        //    obligMember: item.requiredAttendees.map(function (address) { return address.emailAddress; }),
-        //    optionalMember: item.optionalAttendees.map(function (address) { return address.emailAddress; }),
-        //    place: item.location,
-        //    agenda: body,
-        //    type: "OutlookConfluence"
-        //};
-        //console.log(sendData);
-
-        //var siteurl = "https://office.beeline.kz/sites/system";
-        //var soapEnv =
-        //    '<?xml version="1.0" encoding="utf-8"?> \
-        //    <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" \
-        //        xmlns:xsd="http://www.w3.org/2001/XMLSchema" \
-        //        xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"> \
-        //      <soap:Body> \
-        //        <UpdateListItems xmlns="http://schemas.microsoft.com/sharepoint/soap/"> \
-        //          <listName>TMP</listName> \
-        //          <updates> \
-        //            <Batch OnError="Continue"> \
-        //        <Method ID="1" Cmd="New"> \
-        //            <Field Name="Title">'+ item.subject + '</Field> \
-        //             <Field Name="Body">'+ body + '</Field> \
-        //                  </Method> \
-        //    </Batch> </updates> \
-        //        </UpdateListItems> \
-        //      </soap:Body> \
-        //    </soap:Envelope>';
-
-        //$.ajax({
-        //    url: siteurl + "/SitePages/Домашняя.aspx",
-        //    method: 'GET',
-        //    xhrFields: { withCredentials: true },
-        //    success: function (data) {
-        //        var digest = $('input#__REQUESTDIGEST', $(data)).val();
-        //        $.ajax({
-        //            url: siteurl + "/_vti_bin/Lists.asmx",
-        //            type: "POST",
-        //            dataType: "xml",
-        //            xhrFields: { withCredentials: true },
-        //            contentType: 'text/xml; charset="utf-8"',
-        //            headers: {
-        //                "X-RequestDigest": digest
-        //            },
-        //            data: soapEnv,
-        //            complete: console.log,
-        //            success: console.log,
-        //            error: console.log
-        //        });
-        //    }
-        //});
-        //$.ajax({
-        //    url: 'https://confluence.beeline.kz/ajax/confiforms/rest/save.action',
-        //    //url: 'http://localhost:3000',
-        //    type: 'POST',
-        //    contentType: "application/x-www-form-urlencoded",
-        //    data: sendData,
-        //}).always(function (data) { $('#result').html(data); });
-
-        //var data = 'https://confluence.beeline.kz/ajax/confiforms/rest/save.action?' +
-        //    'pageId=53811457' +
-        //    '&f=meetingCollector' +
-        //    '&title01=' + item.subject +
-        //    '&beginTm=' + item.start.toLocaleString('de-DE') +
-        //    '&endTm=' + item.end.toLocaleString('de-DE') +
-        //    '&obligMember=' + item.requiredAttendees.map(function (address) { return address.emailAddress; }) +
-        //    '&optionalMember=' + item.optionalAttendees +
-        //    '&place=' + item.location +
-        //    '&agenda=' + encodeURI(body) +
-        //    '&type=OutlookConfluence';
-        //$.get(data).fail(
-        //        function (xhr, status, error) {
-        //            console.log(error);
-        //            $('#result').html(error);
-        //        });
-    });
-}
+    }
 
     // Вспомогательная функция для отображения уведомлений
     function showNotification(header, content) {
-    $("#notificationHeader").text(header);
-    $("#notificationBody").html(content);
-    messageBanner.showBanner();
-    messageBanner.toggleExpansion();
-}
-}) ();
+        $("#notificationHeader").text(header);
+        $("#notificationBody").html(content);
+        messageBanner.showBanner();
+        messageBanner.toggleExpansion();
+    }
+})();
